5e4863d4899e8594df618cdf6195ad0d0619dbcf
Gadzhi Kharkharov <me@kkga.me>
2021-01-21 02:14:44 +0300

    update atreus keymap

--- keyboards/keyboardio/atreus/keymaps/kkga/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/kkga/keymap.c
@@ -1,43 +1,48 @@
-// Copyright (C) 2019, 2020  Keyboard.io, Inc
-//
-// this is the style you want to emulate.
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+// Copyright 2021 Gadzhi Kharkharov
 
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _QW,
-    _RS,
-    _LW,
+    _QWR,
+    _SYM,
+    _NAV
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+#define SFT_Z SFT_T(KC_Z)
+#define SFT_SLSH SFT_T(KC_SLSH)
+#define NAV_ESC LT(_NAV, KC_ESC)
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_RS] = LAYOUT( /* [> RAISE <] */
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWR] = LAYOUT( /* Qwerty */
+//-------------------------------------------------------//-------------------------------------------------------//
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,     KC_I,    KC_O,    KC_P    ,
+//-------------------------------------------------------//-------------------------------------------------------//
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN ,
+//-------------------------------------------------------//-------------------------------------------------------//
+    SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,  KC_BSLS, KC_N,    KC_M,     KC_COMM, KC_DOT,  SFT_SLSH,
+//-------------------------------------------------------//-------------------------------------------------------//
+    NAV_ESC, KC_TAB,  KC_LGUI, KC_LGUI, KC_SPC,  KC_LCTL, KC_LALT, KC_BSPC, MO(_SYM), KC_MINS, KC_QUOT, KC_ENT ),
+//-------------------------------------------------------//-------------------------------------------------------//
+
+  [_SYM] = LAYOUT( /* [> RAISE <] */
+//-------------------------------------------------------//-------------------------------------------------------//
     KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PERC,                  KC_MINS, KC_7,    KC_8,   KC_9, KC_EQL,
+//-------------------------------------------------------//-------------------------------------------------------//
     KC_HASH, KC_DLR,  KC_DOWN, KC_RGHT, KC_RPRN,                  KC_UNDS, KC_4,    KC_5,   KC_6, KC_PLUS,
+//-------------------------------------------------------//-------------------------------------------------------//
     KC_PERC, KC_CIRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
+//-------------------------------------------------------//-------------------------------------------------------//
     TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LW] = LAYOUT( /* [> LOWER <] */
+//-------------------------------------------------------//-------------------------------------------------------//
+
+  [_NAV] = LAYOUT( /* [> LOWER <] */
+//-------------------------------------------------------//-------------------------------------------------------//
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+//-------------------------------------------------------//-------------------------------------------------------//
+    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_LEFT, KC_DOWN,   KC_UP,   KC_RIGHT,   KC_F11  ,
+//-------------------------------------------------------//-------------------------------------------------------//
+    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_HOME,   KC_PGDN,   KC_PGUP,  KC_END,   KC_F12  ,
+//-------------------------------------------------------//-------------------------------------------------------//
     KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+//-------------------------------------------------------//-------------------------------------------------------//
 };
