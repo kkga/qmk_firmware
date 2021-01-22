3f1d0de1d42581ac3dc31a2b892a0302edf066fb
Gadzhi Kharkharov <me@kkga.me>
2021-01-22 11:45:25 +0300

    update atreus keymap

--- keyboards/keyboardio/atreus/keymaps/kkga/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/kkga/keymap.c
@@ -10,11 +10,13 @@ enum layers {
 };
 
 enum {
-  TD_SCLN_CLN = 0
+  TD_SCLN_CLN,
+  TD_QUO_DQUO
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_SCLN_CLN] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
+    [TD_QUO_DQUO] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
 };
 
 #define SFT_Z SFT_T(KC_Z)
@@ -25,23 +27,24 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define COPY C(KC_C)
 #define PASTE C(KC_V)
 #define SCL_CL TD(TD_SCLN_CLN)
+#define QUO_DQUO TD(TD_QUO_DQUO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
   ,--------------------------------------------.                  ,--------------------------------------------.
   |   q    |   w    |   e    |   r    |   t    |                  |   y    |   u    |   i    |   o    |   p    |
   |--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------|
-  |   a    |   s    |   d    |   f    |   g    |                  |   h    |   j    |   k    |   l    |   '    |
+  |   a    |   s    |   d    |   f    |   g    |                  |   h    |   j    |   k    |   l    |  ;_:   |
   |--------+--------+--------+--------+--------+--------,,--------+--------+--------+--------+--------+--------|
   | Sft/z  |   x    |   c    |   v    |   b    |  Tab   ||        |   n    |   m    |   ,    |   .    | Sft//  |
   |--------+--------+--------+--------+--------+--------||--------+--------+--------+--------+--------+--------|
-  | NAV/Esc|  Tab   |  Alt   |  Sup   |  Spc   |  Ctl   ||  Alt   |  BkSp  |  SYM   |        |  :/;   |  Ent   |
+  | NAV/Esc|  Tab   |  Alt   |  Sup   |  Spc   |  Ctl   ||  Alt   |  BkSp  |  SYM   |        |  '_"   |  Ent   |
   `-----------------------------------------------------''-----------------------------------------------------' */
   [_DEF] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,     KC_K,    KC_L,    KC_QUOT,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,     KC_K,    KC_L,    SCL_CL,
     SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,  _______, KC_N,    KC_M,     KC_COMM, KC_DOT,  SFT_SLSH,
-    NAV_ESC, KC_TAB,  KC_LALT, KC_LGUI, KC_SPC,  KC_LCTL, KC_LALT, KC_BSPC, MO(_SYM), _______, SCL_CL,  KC_ENT ),
+    NAV_ESC, KC_TAB,  KC_LALT, KC_LGUI, KC_SPC,  KC_LCTL, KC_LALT, KC_BSPC, MO(_SYM), _______, QUO_DQUO,KC_ENT ),
 
   /* Symbols and numbers
   ,--------------------------------------------.                  ,--------------------------------------------.
