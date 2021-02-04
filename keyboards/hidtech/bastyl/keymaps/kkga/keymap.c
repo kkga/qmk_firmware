20bc94c982a15ba6ccb9dab2738f7e38f05d9042
Gadzhi Kharkharov <me@kkga.me>
2021-02-04 15:45:14 +0300

    update bastyl

--- keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
@@ -25,39 +25,44 @@ enum layers {
   _RSE
 };
 
-#define     CTL_ESC  CTL_T(KC_ESC)
+#define      CTL_ESC  CTL_T(KC_ESC)
+#define      CTL_TAB  CTL_T(KC_TAB)
+#define      OSM_SFT  OSM(MOD_LSFT)
+#define      RSE_SCLN LT(_RSE, KC_SCLN)
+#define      RSE_ESC  LT(_RSE, KC_ESC)
 
-#define     RAISE    MO(_RSE)
-#define     LOWER    MO(_LWR)
+#define      RAISE    MO(_RSE)
+#define      LOWER    MO(_LWR)
 
-#define     UNDO     C(KC_Z)
-#define     CUT      C(KC_X)
-#define     COPY     C(KC_C)
-#define     PASTE    C(KC_V)
+#define      UNDO     C(KC_Z)
+#define      REDO     C(S(KC_Z))
+#define      CUT      C(KC_X)
+#define      COPY     C(KC_C)
+#define      PASTE    C(KC_V)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWR] = LAYOUT(
-    KC_BCTK, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    RSE_SCLN,KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                               RAISE,   KC_SPC,  KC_BSPC,          KC_DEL,  KC_ENT,  LOWER,
-                                        KC_LGUI, OSM_SFT,          KC_RALT, KC_RGUI
+                               RSE_ESC, KC_SPC,  CTL_TAB,          KC_ENT,  KC_BSPC, LOWER,
+                                        KC_LGUI, OSM_SFT,          KC_RALT, KC_RALT
   ),
   [_LWR] = LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-    RESET,   _______, _______, _______, _______, KC_LBRC,	       KC_RBRC, KC_P7,   KC_P8,   KC_P9,   _______, KC_PLUS,
-    _______, _______, _______, _______, _______, KC_LPRN,	       KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS, KC_PIPE,
-    _______, _______, _______, _______, _______, _______,          KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_EQL,  _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_GRV, 	       KC_MINS, KC_7,    KC_8,    KC_9,    KC_UNDS, _______,
+    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD,          KC_PLUS, KC_4,    KC_5,    KC_6,    KC_EQL,  _______,
+    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR,          KC_0,    KC_1,    KC_2,    KC_3,    KC_ASTR, _______,
                                _______, _______, _______,          _______, _______, XXXXXXX,
-                                        _______, _______,          _______, _______,
+                                        _______, _______,          _______, _______
   ),
   [_RSE] = LAYOUT(
-    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, KC_BRGT, KC_VOLD, KC_MUTE, KC_VOLU, _______,          UNDO,    CUT,     COPY,    PASTE,   _______, _______,
-    _______, KC_BRGT, KC_PREV, KC_PLAY, KC_NEXT, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______, KC_BRIU, KC_VOLD, KC_MUTE, KC_VOLU, _______,          UNDO,    CUT,     COPY,    PASTE,   REDO,    KC_F12,
+    _______, KC_BRID, KC_MPRV, KC_MPLY, KC_MNXT, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
     _______, _______, _______, _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
                                XXXXXXX, _______, _______,          _______, _______, _______,
-                                        _______, _______,          _______, _______,
+                                        _______, _______,          _______, _______
   ),
 };
