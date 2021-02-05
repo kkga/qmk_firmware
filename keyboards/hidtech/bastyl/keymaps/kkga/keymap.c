bbffbdce844846f3b554f320832f2deded1b74e6
Gadzhi Kharkharov <me@kkga.me>
2021-02-05 09:39:50 +0300

    update bastyl

--- keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    RSE_SCLN,KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                               RSE_ESC, KC_SPC,  CTL_TAB,          KC_ENT,  KC_BSPC, LOWER,
+                               RSE_ESC, KC_SPC,  CTL_TAB,          KC_BSPC, KC_ENT,  LOWER,
                                         KC_LGUI, OSM_SFT,          KC_RALT, KC_RALT
   ),
   [_LWR] = LAYOUT(
