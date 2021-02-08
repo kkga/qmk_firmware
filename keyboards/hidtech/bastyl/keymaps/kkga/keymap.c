7653fb183a41307bd1118b98dad5cf02bc0e9293
Gadzhi Kharkharov <me@kkga.me>
2021-02-08 11:58:02 +0300

    update bastyl

--- keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
@@ -22,10 +22,12 @@
 enum layers {
   _QWR,
   _LWR,
-  _RSE
+  _RSE,
+  _ADJ
 };
 
 #define      CTL_ESC  CTL_T(KC_ESC)
+#define      GUI_ESC  GUI_T(KC_ESC)
 #define      CTL_TAB  CTL_T(KC_TAB)
 #define      OSM_SFT  OSM(MOD_LSFT)
 #define      RSE_SCLN LT(_RSE, KC_SCLN)
@@ -34,14 +36,19 @@ enum layers {
 #define      LWR_ESC  LT(_LWR, KC_ESC)
 #define      LWR_DEL  LT(_LWR, KC_DEL)
 
+#define      C_LEFT   C(KC_LEFT)
+#define      C_RGHT   C(KC_RGHT)
+#define      C_BSPC   C(KC_BSPC)
+
 #define      RAISE    MO(_RSE)
 #define      LOWER    MO(_LWR)
+#define      ADJUST   MO(_ADJ)
 
-#define      UNDO     C(KC_Z)
+/* #define      UNDO     C(KC_Z) */
+/* #define      CUT      C(KC_X) */
+/* #define      COPY     C(KC_C) */
+/* #define      PASTE    C(KC_V) */
 #define      REDO     C(S(KC_Z))
-#define      CUT      C(KC_X)
-#define      COPY     C(KC_C)
-#define      PASTE    C(KC_V)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWR] = LAYOUT(
@@ -49,22 +56,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    RSE_SCLN,KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                               KC_LGUI, KC_SPC,  RSE_TAB,          LWR_DEL, KC_ENT,  KC_BSPC,
-                                        KC_LCTL, OSM_SFT,          KC_RALT, KC_RALT
+                               GUI_ESC, KC_SPC,  RSE_TAB,          LWR_DEL, KC_ENT,  KC_BSPC,
+                                        OSM_SFT, KC_LCTL,          KC_RALT, KC_RALT
   ),
   [_LWR] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_GRV, 	       KC_MINS, KC_7,    KC_8,    KC_9,    KC_PLUS, _______,
+    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_GRV, 	       KC_MINS, KC_7,    KC_8,    KC_9,    KC_0,    _______,
     _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD,          KC_UNDS, KC_4,    KC_5,    KC_6,    KC_EQL,  _______,
-    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR,          KC_0,    KC_1,    KC_2,    KC_3,    KC_ASTR, _______,
-                               _______, _______, _______,          _______, _______, _______,
+    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR,          KC_PLUS, KC_1,    KC_2,    KC_3,    KC_ASTR, _______,
+                               _______, _______, ADJUST,           _______, _______, _______,
                                         _______, _______,          _______, _______
   ),
   [_RSE] = LAYOUT(
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, KC_BRIU, KC_VOLD, KC_MUTE, KC_VOLU, _______,          KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,REDO,    KC_F12,
-    _______, KC_BRID, KC_MPRV, KC_MPLY, KC_MNXT, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
-    _______, _______, _______, _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_DEL,
+    _______, KC_PSCR, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRIU,          C_LEFT,  XXXXXXX, XXXXXXX, C_RGHT,  C_BSPC,  KC_F12,
+    _______, KC_APP,  KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, XXXXXXX,
+    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,REDO,             KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_DEL,
+                               _______, _______, _______,          _______, _______, _______,
+                                        _______, _______,          _______, _______
+  ),
+  [_ADJ] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    RESET,   _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
                                _______, _______, _______,          _______, _______, _______,
                                         _______, _______,          _______, _______
   ),
