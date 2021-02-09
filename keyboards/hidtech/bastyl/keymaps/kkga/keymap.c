817d22488aa1cb35c560a0e58d00f14867541e24
Gadzhi Kharkharov <me@kkga.me>
2021-02-09 23:25:03 +0300

    update bastyl

--- keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/kkga/keymap.c
@@ -1,20 +1,6 @@
-// vim:colorcolumn=4,13,22,31,40,49,58,67,76,85,94,103,112,121
+/* vim:colorcolumn=4,13,22,31,40,49,58,67,76,85,94,103,112,121 */
 /*
- * Copyright 2020 Quentin LEBASTARD <qlebastard@gmail.com>
- * Copyright 2020 Anthony MARIN <anthony@hidtech.ca>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2021 Gadzhi KHARKHAROV <me@kkga.me>
  */
 
 #include QMK_KEYBOARD_H
