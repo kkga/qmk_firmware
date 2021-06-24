1f2ebad9e16f235178ea6eb997b43547ea3523d9
Gadzhi Kharkharov <me@kkga.me>
2021-06-24 11:44:16 +0300

    configure mouse keys on bastyl

--- keyboards/hidtech/bastyl/keymaps/kkga/config.h
+++ keyboards/hidtech/bastyl/keymaps/kkga/config.h
@@ -9,3 +9,13 @@
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 #define PERMISSIVE_HOLD
+
+
+// #define MOUSEKEY_MOVE_DELTA 2
+#define MOUSEKEY_WHEEL_DELAY 50
+#define MOUSEKEY_WHEEL_INTERVAL 50
+#define MOUSEKEY_WHEEL_MAX_SPEED 8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+// #define MK_KINETIC_SPEED
+// #define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 1
